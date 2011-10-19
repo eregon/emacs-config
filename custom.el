147befa0a78788ab856c227362420ca56b5fb365
eregon <eregontp@gmail.com>
2011-10-19 11:30:09 +0200

    rem comment

--- custom.el
+++ custom.el
@@ -13,4 +13,4 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/plugins-conf/yasnippet/snippets") ; "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets"
+(yas/load-directory "~/.emacs.d/plugins-conf/yasnippet/snippets")
